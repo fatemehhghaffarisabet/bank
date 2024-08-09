d49c4174fc5fffc600635133b31681ec1d7dcf5b
Fatemeh Ghaffari Sabet <fatemehh.ghaffarisabet@gmail.com>
2024-08-09 14:32:54 +0330

    create person and account models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+    ssn = models.CharField(max_length=10, validators=[MinLengthValidator(10)])
+
+class Account(models.Model):
+    balance = models.PositiveIntegerField(default=0)
+    owner = models.ForeignKey(Person, on_delete=models.PROTECT)
